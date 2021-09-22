1a514e2d25422a8f7c5dfdb673750d257672cfc7
Bogey <jeffreysbogart@gmail.com>
2021-09-22 00:29:42 -0700

    Finish calculator function

--- src/calculator.js
+++ src/calculator.js
@@ -1,68 +1,70 @@
 const calculate = (input) => {
 
-  const ops = {
-    add: '+',
-    sub: '-',
-    div: '/',
-    mlt: '*'
-  };
+  let output;
 
-  // Create array for Order of Operation and precedence
-  ops.opOrder = [
+  const opOrder = [
     [
-      [ops.mlt],
-      [ops.div]
+      ['*'],
+      ['/']
     ],
     [
-      [ops.add],
-      [ops.sub]
+      ['+'],
+      ['-']
     ]
   ];
 
   const characterTest = input.replace(/[^0-9*\/()\-+.\s]/g, '');
   if (characterTest !== input) {
-    return "Invalid Input"
+    return "Invalid Input";
   }
 
-  input = input.replace(/[^0-9*\/()\-+.]/g, ''); // clean up unnecessary characters
+  input = input.replace(/[^0-9*\/()\-+.]/g, '');
 
-  const evaluate = (a, op, b) => {
-    a = a * 1;
-    b = b * 1;
-    switch (op) {
-      case ops.add:
-        return a + b;
-        break;
-      case ops.sub:
-        return a - b;
-        break;
-      case ops.div:
-        return a / b;
-        break;
-      case ops.mlt:
-        return a * b;
-        break;
-      default:
-        null;
-    }
+  const opTest = /(\+|-|\*|\/)(\+|\*|\/)/
+  if (opTest.test(input)) {
+    return "Syntax Error";
   }
 
-  let output;
+  const para = new RegExp('([(])(.*)([)])')
+  while (para.test(input)) {
+    output = calculate(RegExp.$2);
+    if (isNaN(output) || !isFinite(output))
+      return "Syntax Error";
+    input = input.replace(para, output);
+  }
   
-  for (let i = 0, n = ops.opOrder.length; i < n; i++) {
+  for (let i = 0; i < opOrder.length; i++) {
+    let exp = new RegExp('(-?\\.?\\d+\\.?\\d*)([\\' + opOrder[i].join('\\') + '])(-?\\.?\\d+\\.?\\d*)');
+    exp.lastIndex = 0;
 
-    // Regular Expression to look for operators between floating numbers or integers
-    let re = new RegExp('(-?\\d+\\.?\\d*)([\\' + ops.opOrder[i].join('\\') + '])(-?\\d+\\.?\\d*)');
-    re.lastIndex = 0; // take precautions and reset re starting pos
-
-    // Loop while there is still calculation for level of precedence
-    while (re.test(input)) {
+    while (exp.test(input)) {
       output = evaluate(RegExp.$1, RegExp.$2, RegExp.$3);
       if (isNaN(output) || !isFinite(output))
-        return output; // exit early if not a number
-      input = input.replace(re, output);
+        return output;
+      input = input.replace(exp, output);
     }
   }
 
   return output;
+}
+
+const evaluate = (a, op, b) => {
+  a = a * 1;
+  b = b * 1;
+  switch (op) {
+    case '+':
+      return a + b;
+      break;
+    case '-':
+      return a - b;
+      break;
+    case '/':
+      return a / b;
+      break;
+    case '*':
+      return a * b;
+      break;
+    default:
+      null;
+  }
 }
